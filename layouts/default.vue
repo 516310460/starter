9b041f592d1852280148a5a5e1a126a37dec2d63
jakeWangHong <516310460@qq.com>
2023-01-16 16:11:59 +0800

    添加模版

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,13 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <div class="h-full">
+  <div
+    class="h-full"
+    v-waterMarker="{
+      text: 'Bitget Nuxt3 template',
+      textColor: 'rgba(16, 185, 129, 0.3)'
+  }"
+  >
     <header
       class="flex items-center justify-center"
       h="10"
